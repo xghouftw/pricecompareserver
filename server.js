e932e51f3dac917006023b724e8fbd34f4e18f9e
XGHouE <connor.b.zhang.27@dartmouth.edu>
2025-01-24 17:27:23 -0500

    port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use('/kroger', krogerRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
